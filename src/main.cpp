82e79b468a2f11cf59d96d97d3d1faee973ef786
mechaminu <lmw5201@gmail.com>
2022-09-01 21:58:08 +0900

    3passive1active driver

--- src/main.cpp
+++ src/main.cpp
@@ -8,35 +8,35 @@ BLDCMotor motor = BLDCMotor(7);
 BLDCDriver6PWM driver = BLDCDriver6PWM(10, 11, 12, 13, 16, 17);
 MagneticSensorSPI sensor = MagneticSensorSPI(AS5147_SPI, 5);
 
+
 float xAng, yAng, zAng, elbowAng;
 float x, y, z, e;
 float _x, _y, _z;
-unsigned long prevTime;
+
+float prevTime, curTime;
 
 float getAngI2C(int addr) {
   _wire.beginTransmission(addr);
   _wire.write(0x0E);
   _wire.endTransmission();
   _wire.requestFrom(addr, 2);
-  int resBytes = _wire.available() >= 2 ? (_wire.read() << 8) | _wire.read() : 0;
+  int resBytes = (_wire.available() >= 2) ? ((_wire.read() << 8) | _wire.read()) : 0;
   return (float)resBytes / 4096 * 360;
 }
 
+float res;
 void torqueCommand() {
-    if (Serial.available()) {
+    while (Serial.available()) {
       if (Serial.read() == 0x02 && Serial.read() == 0xFF) {
-        float res;
         Serial.readBytesUntil(0x04, (byte*)&res, 4);
         motor.target = res / 1.333;
-      } else {
-        // motor.target = 0;
       }
     }
-    while(Serial.available()) Serial.read();
 }
 
 void setup() {
   _wire.begin();
+  _wire.setTimeout(10);
   sensor.init(&_spi);
   motor.linkSensor(&sensor);
 
@@ -56,7 +56,7 @@ void setup() {
   motor.voltage_sensor_align = 1;
   motor.velocity_limit = 50;
 
-  Serial.begin(115200);
+  Serial.begin(921600);
   motor.init();
   motor.initFOC(6.25, Direction::CCW);
   motor.sensor_offset = -4.415;
@@ -73,12 +73,11 @@ void setup() {
   prevTime = millis();
 }
 
-
 byte buf[2+4*4];
-
 void loop() {
-
-  unsigned long curTime = millis();
+  torqueCommand();
+  motor.loopFOC();
+  motor.move();
 
   _x = getAngI2C(0x40);
   _y = getAngI2C(0x41);
@@ -91,18 +90,16 @@ void loop() {
   y = _y;
   z = _z;
 
-  if (curTime - prevTime > 10) {
+  curTime = millis();
+
+  if (curTime - prevTime > 17) {
     buf[0] = 0x02;
     memcpy(&buf[1+4*(1-1)],&zAng, 4);
     memcpy(&buf[1+4*(2-1)],&yAng, 4);
     memcpy(&buf[1+4*(3-1)],&xAng, 4);
     memcpy(&buf[1+4*(4-1)],&elbowAng, 4);
     buf[2+4*4-1] = 0x04;
-    Serial.write(buf,18); 
+    Serial.write(buf,18);
     prevTime = curTime;
   }
-
-  motor.loopFOC();
-  motor.move();
-  torqueCommand();
 }
