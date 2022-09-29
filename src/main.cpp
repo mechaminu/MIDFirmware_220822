d6eca03c4c7cd4b1c0d9c39fda1e2a0deacba09a
mechaminu <lmw5201@gmail.com>
2022-09-29 20:15:31 +0900

    wire debugging

--- src/main.cpp
+++ src/main.cpp
@@ -1,105 +1,130 @@
+#include "mbed.h"
 #include <Arduino.h>
 #include <SimpleFOC.h>
 
-MbedSPI _spi(4,3,2);
-MbedI2C _wire(26,27);
-
+MbedSPI spi(4,3,2);
+MbedI2C wire(16,17);
 BLDCMotor motor = BLDCMotor(7);
-BLDCDriver6PWM driver = BLDCDriver6PWM(10, 11, 12, 13, 16, 17);
+BLDCDriver6PWM driver = BLDCDriver6PWM(8, 11, 9, 12, 10, 13);
 MagneticSensorSPI sensor = MagneticSensorSPI(AS5147_SPI, 5);
 
-
 float xAng, yAng, zAng, elbowAng;
-float x, y, z, e;
-float _x, _y, _z;
+float _x, _y, _z, _e;
+unsigned long prevTime;
 
-float prevTime, curTime;
-
-float getAngI2C(int addr) {
-  _wire.beginTransmission(addr);
-  _wire.write(0x0E);
-  _wire.endTransmission();
-  _wire.requestFrom(addr, 2);
-  int resBytes = (_wire.available() >= 2) ? ((_wire.read() << 8) | _wire.read()) : 0;
-  return (float)resBytes / 4096 * 360;
+int getAngI2C(int addr, float& ang) {
+  wire.requestFrom(addr, 6);
+  delayMicroseconds(20);
+  while (wire.available() > 0) {
+    if (wire.read() == 0x02) {
+      wire.readBytesUntil(0x04,(uint8_t*)&ang,4);
+      return 1;
+    }
+  }
+  return 0;
 }
 
-float res;
+void getAng(int addr, float &target, float offset, float ratio) {
+  float res;
+  if (getAngI2C(addr,res))
+    target = (res - offset) / PI * 180 / ratio;
+}
+
+byte buf_tq[16];
+float xTorque, eTorque;
 void torqueCommand() {
-    while (Serial.available()) {
-      if (Serial.read() == 0x02 && Serial.read() == 0xFF) {
-        Serial.readBytesUntil(0x04, (byte*)&res, 4);
-        motor.target = res / 1.333;
-      }
+  while (Serial.available() > 0) {
+    if (Serial.read() == 0x02) {
+      switch (Serial.read()) {
+        case 0xFF:
+          Serial.readBytesUntil(0x04, buf_tq, 16);
+          memcpy(&xTorque,&buf_tq[4*2],4);
+          memcpy(&eTorque,&buf_tq[4*3],4);
+      } 
     }
+  }
+  motor.target = eTorque / 1.33;
+
+  uint8_t buf[6];
+  buf[0] = 0x02;
+  memcpy(&buf[1],(uint8_t*)&xTorque,4);
+  buf[5] = 0x04;
+
+  wire.beginTransmission(0x01);
+  wire.write(buf,6);
+  Serial.println(wire.endTransmission());
 }
 
 void setup() {
-  _wire.begin();
-  _wire.setTimeout(10);
-  sensor.init(&_spi);
+
+  for (int i = 0; i < 5; i++) {
+    digitalWrite(17,HIGH);
+    digitalWrite(16,HIGH);
+    delay(500);
+    digitalWrite(17,LOW);
+    digitalWrite(16,LOW);
+    delay(500);
+  }
+
+  wire.begin();
+
+  Serial.begin(115200);
+  // motor.useMonitoring(Serial);
+
+  sensor.init(&spi);
   motor.linkSensor(&sensor);
 
-  driver.pwm_frequency = 20000;
   driver.voltage_power_supply = 12;
   driver.voltage_limit = 4;
   driver.init();
-
   motor.linkDriver(&driver);
+
+  motor.voltage_sensor_align = 1;
   motor.foc_modulation = FOCModulationType::SpaceVectorPWM;
   motor.controller = MotionControlType::torque;
-  motor.PID_velocity.P = 0.3f;
-  motor.PID_velocity.I = 10;
-  motor.PID_velocity.D = 0;
-  motor.LPF_velocity.Tf = 0.01;
-  motor.P_angle.P = 20;
-  motor.voltage_sensor_align = 1;
-  motor.velocity_limit = 50;
 
-  Serial.begin(921600);
   motor.init();
-  motor.initFOC(6.25, Direction::CCW);
-  motor.sensor_offset = -4.415;
+  // motor.initFOC();
+  motor.initFOC(5.78, Direction::CCW);
   motor.target = 0;
 
-  x = getAngI2C(0x40);
-  y = getAngI2C(0x41);
-  z = getAngI2C(0x42);
-  e = sensor.getAngle();
-  xAng = 0;
-  yAng = 0;
-  zAng = 0;
+  // Initial value for zeroing
+  getAngI2C(0x01, _x);
+  _e = sensor.getAngle();
+  _y = 0;
+  _z = 0;
 
-  prevTime = millis();
+  // getAngI2C(0x02, _y);
+  // getAngI2C(0x03, _z);
 }
 
 byte buf[2+4*4];
 void loop() {
-  torqueCommand();
+  
   motor.loopFOC();
   motor.move();
 
-  _x = getAngI2C(0x40);
-  _y = getAngI2C(0x41);
-  _z = getAngI2C(0x42);
-  xAng += x - _x;
-  yAng -= y - _y;
-  zAng += z - _z;
-  elbowAng = -(sensor.getAngle() - e)/PI*180/5;
-  x = _x; 
-  y = _y;
-  z = _z;
+  getAng(0x01, xAng, _x, -40);
+  elbowAng = -(sensor.getAngle() - _e)/PI*180/5;
+  // getAng(0x02, yAng, _y, 1);
+  // getAng(0x03, zAng, _z, -40);
 
-  curTime = millis();
+  Serial.print(elbowAng);
+  Serial.print('\t');
+  Serial.print(xAng);
+  Serial.print('\n');
 
-  if (curTime - prevTime > 17) {
+  unsigned long curTime = millis();
+  if (curTime - prevTime > 20) {
     buf[0] = 0x02;
     memcpy(&buf[1+4*(1-1)],&zAng, 4);
     memcpy(&buf[1+4*(2-1)],&yAng, 4);
     memcpy(&buf[1+4*(3-1)],&xAng, 4);
     memcpy(&buf[1+4*(4-1)],&elbowAng, 4);
     buf[2+4*4-1] = 0x04;
-    Serial.write(buf,18);
+    // Serial.write(buf,18);
+    torqueCommand();
+
     prevTime = curTime;
   }
 }
